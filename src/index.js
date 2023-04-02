6166b449e98e798f4f6e707adeb805b73f65b031
SergeiMironenko <90122255+SergeiMironenko@users.noreply.github.com>
2023-04-02 23:48:59 +0700

    add plotly.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Plotly from 'plotly.js-dist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// var Plotly = require('plotly.js-dist');
+const TESTER = document.getElementById('tester');
+Plotly.newPlot( TESTER, [{
+x: [1, 2, 3, 4, 5],
+y: [1, 2, 4, 8, 16] }], {
+margin: { t: 0 } } );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
